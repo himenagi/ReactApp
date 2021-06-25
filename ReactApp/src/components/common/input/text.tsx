61db31781c101b318b4ea8fee740c320bff383bd
Tomonari-PC\Tomonari <Himenagi1201@gmail.com>
2021-06-25 21:56:20 +0900

    テキストボックス共通部品に最小限のuseState、useEffectを追加

--- ReactApp/src/components/common/input/text.tsx
+++ ReactApp/src/components/common/input/text.tsx
@@ -1,14 +1,22 @@
 declare var require: any
 var React = require('react');
 
+import { useState, useEffect } from 'react';
 import TextField from '@material-ui/core/TextField';
 
 // テキストボックス共通部品
 export default (props: PropsObject) => {
+    const [value, setValue] = useState(props.value || "");
+
+    useEffect(() => {
+        setValue(props.value);
+    }, [props.value])
+
     return (
         <TextField
             variant="outlined"
-            value={props.value}
+            value={value}
+            onChange={e => setValue(e.target.value)}
         />
     );
 }
